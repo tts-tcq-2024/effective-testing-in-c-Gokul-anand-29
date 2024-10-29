0429f0e2ee9b6e13d4374ae417fb86c1c3612d1a
Gokul-anand-29 <gokulanand200@gmail.com>
2024-10-29 11:57:18 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,59 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-int printColorMap() {
+void printOnConsole(const char* manualItem){
+    printf("%s", manualItem);
+}
+int printColorMap(void(*fp)(const char*)) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            fp("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
 }
+char actualManual;
+void mockPrintOnConsole(const char* manualItem)
+{
+    actualManual += manualItem;
+}
 
 int main() {
-    int result = printColorMap();
+    int result = printColorMap(&mockPrintOnConsole);
     assert(result == 25);
+
+    const char* expectedConsoleBuffer =
+        "0 | White | Blue\n"
+        "1 | White | Orange\n"
+        "2 | White | Green\n"
+        "3 | White | Brown\n"
+        "4 | White | Slate\n"
+        "5 | Red | Blue\n"
+        "6 | Red | Orange\n"
+        "7 | Red | Green\n"
+        "8 | Red | Brown\n"
+        "9 | Red | Slate\n"
+        "10 | Black | Blue\n"
+        "11 | Black | Orange\n"
+        "12 | Black | Green\n"
+        "13 | Black | Brown\n"
+        "14 | Black | Slate\n"
+        "15 | Yellow | Blue\n"
+        "16 | Yellow | Orange\n"
+        "17 | Yellow | Green\n"
+        "18 | Yellow | Brown\n"
+        "19 | Yellow | Slate\n"
+        "20 | Violet | Blue\n"
+        "21 | Violet | Orange\n"
+        "22 | Violet | Green\n"
+        "23 | Violet | Brown\n"
+        "24 | Violet | Slate\n";
+
+    assert(strcmp(actualManual, expectedConsoleBuffer) == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
